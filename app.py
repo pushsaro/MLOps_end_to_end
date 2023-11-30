58e248e5048230904593e17d27144e8bbbe850b2
pushsaro <pushkarsarode1990@gmail.com>
2023-11-30 11:30:24 +0530

    changed file

--- app.py
+++ app.py
@@ -42,6 +42,6 @@ def predict():
         return render_template("index.html", anticipation = output)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
 
