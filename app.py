70939e0ea257fcbf7b1272d7220f97213b3ad7c8
Pushkar Sarode <95062592+pushsaro@users.noreply.github.com>
2023-11-30 14:43:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ def predict():
         return render_template("index.html", anticipation = output)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
     
